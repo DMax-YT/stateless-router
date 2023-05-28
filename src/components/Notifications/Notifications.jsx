f84f3bf6ae59f13c597402042644fda897be98c6
DMax-YT <13312994+DMax-YT@users.noreply.github.com>
2023-05-28 19:13:21 +0300

    move from ToastBar to own implementation

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,21 +1,21 @@
 import { createPortal } from 'react-dom'
-import { ToastBar, Toaster, toast } from 'react-hot-toast'
+import { Toaster, toast, resolveValue } from 'react-hot-toast'
 
 import './Notifications.css'
 
 export const Notifications = () => {
   return createPortal(
     <Toaster position='top-right' toastOptions={{ className: 'toast' }}>
-      {(t) => (
-        <ToastBar toast={t}>
-          {({ icon, message }) => (
-            <div onClick={() => toast.dismiss(t.id)}>
-              {icon}
-              {message}
-            </div>
-          )}
-        </ToastBar>
-      )}
+      {(t) => {
+        const animation = t.visible ? 'toast-in' : 'toast-out'
+        const toastClass = `toast ${animation}`
+
+        return (
+          <div className={toastClass} onClick={() => toast.dismiss(t.id)}>
+            {resolveValue(t.message, t)}
+          </div>
+        )
+      }}
     </Toaster>,
 
     document.body
