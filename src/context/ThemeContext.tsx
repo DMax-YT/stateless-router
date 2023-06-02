6f33f118fa1d592cf0969b4a9e0aa527962d9a93
DMax-YT <13312994+DMax-YT@users.noreply.github.com>
2023-06-02 17:07:19 +0300

    create theme provider

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import { ReactNode, createContext, useState } from 'react'
+
+export type Theme = 'dark' | 'light' | 'auto'
+
+interface ThemeContext {
+  theme: Theme
+  setTheme: (theme: Theme) => void
+}
+
+const ThemeContext = createContext<ThemeContext | null>(null)
+
+const THEME_KEY = 'theme'
+
+const loadTheme = (): Theme => {
+  const savedTheme = localStorage.getItem(THEME_KEY)
+
+  if (!savedTheme) {
+    return window.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light'
+  }
+
+  return savedTheme as Theme
+}
+
+const saveTheme = (theme: Theme) => {
+  localStorage.setItem(THEME_KEY, theme)
+}
+
+export const ThemeProvider = ({ children }: { children?: ReactNode }) => {
+  const [theme, setTheme] = useState(loadTheme)
+
+  const updateTheme = (theme: Theme) => {
+    saveTheme(theme)
+    setTheme(theme)
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme: updateTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
