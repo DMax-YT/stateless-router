a53bdd2fa550f3d0e933c2ad47cac4eb70890ccf
DMax-YT <13312994+DMax-YT@users.noreply.github.com>
2023-06-07 22:52:18 +0300

    fix: change eslint config from `false` to `0`
    Signed-off-by: DMax-YT <13312994+DMax-YT@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    'react/prop-types': false
+    'react/prop-types': 0
   },
 }
